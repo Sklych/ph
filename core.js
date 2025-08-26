7ea4d68fe964f6c5e2ca54c262d2fbbc71e1d5b2
Kostya Zinoview <k.gig@list.ru>
2025-08-26 17:13:05 +0300

    deploy debug version to test

--- core.js
+++ core.js
@@ -3,17 +3,40 @@ if (telegramWA) {
   telegramWA.ready();
 }
 
-const appConfig = {
+const localAppConfig = {
   enableLogs: true,
-  telegramWebApp: telegramWA
-};
+  base_url: "http://127.0.0.1:5001",
+  hasRealTgWebApp: false,
+  telegramWebApp: {
+    initData: null,
+    initDataUnsafe: {
+      start_param: null
+    }
+  }
+}
 
-console.log(`core.js init tg=${JSON.stringify(telegramWA)}`)
+const debugAppConfig = {
+  enableLogs: true,
+  base_url: "https://myphrilldemo.pythonanywhere.com",
+  hasRealTgWebApp: true,
+  telegramWebApp: telegramWA
+}
+
+const prodAppConfig = {
+  enableLogs: false,
+  base_url: "http://127.0.0.1:5001",
+  hasRealTgWebApp: true,
+  telegramWebApp: telegramWA
+}
+
+const appConfig = debugAppConfig;
 
 if (!appConfig.enableLogs) {
   console.log = function () { };
   console.error = function () { };
 }
+
+console.log(`core.js init. appConfig=${JSON.stringify(appConfig)}`)
 
 function versionCompare(v1, v2) {
     if (typeof v1 !== 'string') v1 = '';
@@ -36,25 +59,25 @@ function versionAtLeast(webAppVersion, ver) {
   }
   
   function playHapticNavigation() {
-    if (appConfig.telegramWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
+    if (appConfig.hasRealTgWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
       appConfig.telegramWebApp.HapticFeedback.impactOccurred('soft')
     }
   }
   
   function playHapticSuccess() {
-    if (appConfig.telegramWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
+    if (appConfig.hasRealTgWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
       appConfig.telegramWebApp.HapticFeedback.notificationOccurred('success')
     }
   }
 
   function playHapticWarning() {
-    if (appConfig.telegramWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
+    if (appConfig.hasRealTgWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
       appConfig.telegramWebApp.HapticFeedback.notificationOccurred('warning')
     }
   }
   
   function playHapticError() {
-    if (appConfig.telegramWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
+    if (appConfig.hasRealTgWebApp && versionAtLeast(appConfig.telegramWebApp.version, '6.1')) {
       appConfig.telegramWebApp.HapticFeedback.notificationOccurred('error')
     }
   }
@@ -100,6 +123,101 @@ function showSnackbar(message, iconSrc = null, duration = 3000) {
   }, duration);
 }
 
+class User {
+  constructor(data) {
+    this.transactions = data.transactions || [];
+    this.dailyProfitPercent = data.dailyProfitPercent || 1.03;
+    this.payload = data.payload || ""
+  }
+}
+
+async function getUserSnapshot(initData, ref = null) {
+  const params = new URLSearchParams({ });
+  if (initData) params.append('initData', initData);
+  if (ref) params.append('ref', ref);
+
+  try {
+    const res = await fetch(`${appConfig.base_url}/snapshot?${params.toString()}`, {
+      method: "GET",
+    });
+
+    if (!res.ok) {
+      const text = await res.text();
+      console.error(`Get snapshot failed: ${text}`);
+      return null;
+    }
+
+    const data = await res.json();
+    console.log("Snapshot data:", data);
+
+    return new User(data);
+  } catch (e) {
+    console.error("getUserSnapshot fetch error:", e);
+    return null;
+  }
+}
+
+class Stake {
+  constructor(transactions = [], isAvailable = true) {
+    this.transactions = transactions;
+    this.isAvailable = isAvailable;
+  }
+}
+
+async function stake(initData, { tx_boc, payout_interval, wallet, staked_amount, returned_amount }) {
+  try {
+    const params = new URLSearchParams({ });
+    if (initData) params.append('initData', initData);
+    console.log("client send data ", JSON.stringify({
+      tx_boc,
+      payout_interval,
+      wallet,
+      staked_amount,
+      returned_amount,
+    }), payout_interval)
+    const res = await fetch(`${appConfig.base_url}/stake?${params.toString()}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        tx_boc,
+        payout_interval,
+        wallet,
+        staked_amount,
+        returned_amount,
+      }),
+    });
+
+    if (res.status === 200) {
+      const data = await res.json();
+      return new Stake(data.transactions ?? [], true);
+    }
+
+    if (res.status === 409) {
+      return new Stake([], false);
+    }
+
+    throw new Error(`Stake request failed with status ${res.status}`);
+  } catch (e) {
+    console.error("stake fetch error:", e);
+    throw e;
+  }
+}
+
+async function isStakeAvailable(initData) {
+  try {
+    const params = new URLSearchParams({ });
+    if (initData) params.append('initData', initData);
+    const response = await fetch(`${appConfig.base_url}/checkStake?${params.toString()}`, {
+      method: "GET",
+    });
+
+    return response.status === 200;
+  } catch (error) {
+    console.error("Error checking stake:", error);
+    return false;
+  }
+}
+
 window.showSnackbar = showSnackbar;
 window.appConfig = appConfig;
 window.versionAtLeast = versionAtLeast;
@@ -107,3 +225,8 @@ window.playHapticNavigation = playHapticNavigation;
 window.playHapticSuccess = playHapticSuccess;
 window.playHapticWarning = playHapticWarning;
 window.playHapticError = playHapticError;
+
+// network fcs
+window.getUserSnapshot = getUserSnapshot;
+window.stake = stake;
+window.isStakeAvailable = isStakeAvailable;
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const tabItems = new Map();
 tabItems.set("stake", 0)
 tabItems.set("community", 1)
+tabItems.set("pool", 2)
 
 const profitIntervals = new Map();
 profitIntervals.set("day", 0)
 profitIntervals.set("week", 1)
 profitIntervals.set("month", 2)
 profitIntervals.set("year", 3)
+
+const TxStatus = {
+    completed: 0,
+    pending: 1,
+    success: 2,
+    failure: 3
+}
 
 const appState = {
     sliderValuePercent: 0,
@@ -15,25 +23,28 @@ const appState = {
     connectedWallet: undefined
 };
 
-async function sendTransaction(tonConnectUI, amount) {
+// todo пофиксить баг что стейкинг доступен даже когда уже есть стейкинг в процессе
+async function sendTransaction(tonConnectUI, amount, payload) {
     const transaction = {
-      validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec exp time
-      messages: [
-        {
-          address: "UQDde3w4YGvwyLci1IWtC2COwFj63dnI-b7QbPolGD7_oLNX",
-          amount: amount * 10 ** 9,
-        },
-      ]
+        validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec exp time
+        messages: [
+            {
+                address: "UQDde3w4YGvwyLci1IWtC2COwFj63dnI-b7QbPolGD7_oLNX",
+                amount: amount * 10 ** 9,
+                payload: payload
+            },
+        ]
     };
-  
-    try {
-      return await tonConnectUI.sendTransaction(transaction);
-    } catch (e) {
-      return null;
-    }
-  }
 
-function updateStakingInfo(value) {
+    try {
+        return await tonConnectUI.sendTransaction(transaction);
+    } catch (e) {
+        console.error(e);
+        return null;
+    }
+}
+
+function updateStakingInfo(value, interval) {
     const stakingInfo = document.getElementById('staking-info');
     const now = new Date();
 
@@ -45,24 +56,25 @@ function updateStakingInfo(value) {
         return date.toLocaleDateString('en-US', options);
     }
 
-    const nextDay = new Date(now);
-    nextDay.setDate(now.getDate() + 1);
-
-    const nextWeek = new Date(now);
-    nextWeek.setDate(now.getDate() + 7);
-
-    const nextMonth = new Date(now);
-    nextMonth.setMonth(now.getMonth() + 1);
-
-    const nextYear = new Date(now);
-    nextYear.setFullYear(now.getFullYear() + 1);
+    const future = new Date(now);
+    if (interval == profitIntervals.get("day")) {
+        future.setDate(now.getDate() + 1);
+    } else if (interval == profitIntervals.get("week")) {
+        future.setDate(now.getDate() + 7);
+    } else if (interval == profitIntervals.get("month")) {
+        future.setDate(now.getMonth() + 1);
+    } else if (interval == profitIntervals.get("year")) {
+        future.setFullYear(now.getFullYear() + 1);
+    } else {
+        console.error(`Unknow interval=${interval} is passed to updateStakingInfo`)
+    }
 
     stakingInfo.innerHTML = `
-        You will earn <span class="highlight">${value} TON</span> on ${formatDate(nextDay)} or ${formatDate(nextWeek)} or ${formatDate(nextMonth)} or ${formatDate(nextYear)}
+        You will earn <span class="highlight">${value.toFixed(2)} TON</span> on ${formatDate(future)}
     `;
 }
 
-function initSlider() {
+function initSlider(profitPercent) {
     const slider = document.getElementById('staking-slider');
     const sliderText = document.getElementById('staking-slider-text');
 
@@ -79,20 +91,6 @@ function initSlider() {
         slider.style.background = `linear-gradient(to right, #7CFC00 0%, #7CFC00 ${percentage}%, var(--color-accent) ${percentage}%, var(--color-accent) 100%)`;
     }
 
-    function mapSliderToTON(percent) {
-        if (percent <= 20) {
-            return (0.5 + percent * 0.5).toFixed(2);
-        } else if (percent <= 40) {
-            return (10 + ((percent - 20) * 1)).toFixed(2);
-        } else if (percent <= 60) {
-            return (30 + ((percent - 40) * 3)).toFixed(2);
-        } else if (percent <= 80) {
-            return (90 + ((percent - 60) * 10)).toFixed(2);
-        } else {
-            return (290 + ((percent - 80) * 15)).toFixed(2);
-        }
-    }
-
     function updateGUI(percent) {
         console.log(percent)
         const tonValue = mapSliderToTON(percent);
@@ -100,74 +98,263 @@ function initSlider() {
         appState.stakedValue = tonValue;
         updateSliderText(appState.stakedValue);
         updateSliderBackground(slider);
-        updateStakingInfo(tonValue);
+        updateStakingInfo(tonValue * profitPercent, appState.profitInterval);
     }
 
-    slider.addEventListener('input', (e) => {
+    function onSliderInput(e) {
         const percent = e.target.value;
-        updateGUI(percent)
-        updateStakingInfo(percent);
+        updateGUI(percent);
         playHapticNavigation();
-    });
+    }
+
+    slider.removeEventListener('input', onSliderInput);
+    slider.addEventListener('input', onSliderInput);
 
     slider.value = appState.sliderValuePercent;
     updateGUI(appState.sliderValuePercent);
 }
 
-function updateStakeButton(tonConnectUI) {
-    const stakeButton = document.getElementById("stakeBtn")
-    const isWalletConected = tonConnectUI.wallet
-    console.log('wallet ', isWalletConected)
-    if (isWalletConected) {
+function formatProfitDate(ts) {
+    if (!ts) return "";
+    const date = new Date(ts * 1000); // timestamp in seconds → ms
+    const options = { month: "short", day: "numeric" };
+    return `You’ll receive profit on ${date.toLocaleDateString("en-US", options)}`;
+}
+
+function renderTransactions(transactions) {
+    const list = document.querySelector(".cell-list");
+    list.innerHTML = `<div style="height: 16px"></div>`;
+
+    transactions.forEach(tx => {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+
+        const row = document.createElement("div");
+        row.className = "row main-row";
+
+        const column = document.createElement("div");
+        column.className = "column";
+
+        const title = document.createElement("span");
+        title.className = "title";
+        title.textContent = "Stake";
+        column.appendChild(title);
+
+        const vs = document.createElement("div");
+        vs.className = "vertical-space";
+        column.appendChild(vs);
+
+        const amountsRow = document.createElement("div");
+        amountsRow.className = "row";
+
+        amountsRow.innerHTML = `
+        <span class="text-secondary">${tx.staked_amount.toFixed(2)}</span>
+        <img src="img/ton.svg" alt="ton" class="icon" />
+        <span class="text-secondary">-></span>
+        <span class="text-secondary">${tx.returned_amount.toFixed(2)}</span>
+        <img src="img/ton.svg" alt="ton" class="icon" />
+      `;
+        column.appendChild(amountsRow);
+
+        let rightElement;
+        let iconSrc = "img/pending.svg";
+
+        switch (tx.status) {
+            case TxStatus.completed:
+                iconSrc = "img/completed.svg";
+                rightElement = document.createElement("span");
+                rightElement.className = "view-blockchain";
+                rightElement.textContent = "View in Blockchain";
+                rightElement.addEventListener("click", () => {
+                    window.open(`https://tonviewer.com/${tx.returned_tx_hash}`, "_blank");
+                });
+                break;
+
+            case TxStatus.pending:
+                iconSrc = "img/pending.svg";
+                rightElement = document.createElement("span");
+                rightElement.className = "text-secondary";
+                rightElement.textContent = "Waiting confirmation";
+                break;
+
+            case TxStatus.success:
+                iconSrc = "img/completed.svg";
+                rightElement = document.createElement("span");
+                rightElement.className = "text-secondary";
+                rightElement.textContent = formatProfitDate(tx.time_stamp_profit);
+                break;
+
+            case TxStatus.failure:
+                iconSrc = "img/error.svg";
+                rightElement = document.createElement("span");
+                rightElement.className = "text-secondary";
+                rightElement.textContent = "Transaction was not confirmed";
+                break;
+        }
+
+        const icon = document.createElement("img");
+        icon.src = iconSrc;
+        icon.className = "cell-icon";
+        icon.alt = "status";
+
+        row.appendChild(column);
+        row.appendChild(rightElement);
+
+        cell.appendChild(icon);
+        cell.appendChild(row);
+        list.appendChild(cell);
+    });
+}
+
+function mapSliderToTON(percent) {
+    if (percent <= 20) {
+        return (0.5 + percent * 0.5).toFixed(2);
+    } else if (percent <= 40) {
+        return (10 + ((percent - 20) * 1)).toFixed(2);
+    } else if (percent <= 60) {
+        return (30 + ((percent - 40) * 3)).toFixed(2);
+    } else if (percent <= 80) {
+        return (90 + ((percent - 60) * 10)).toFixed(2);
+    } else {
+        return (290 + ((percent - 80) * 15)).toFixed(2);
+    }
+}
+
+function calculateStakedReturnedAmount(dailyProfitPercent) {
+    const stakedAmount = mapSliderToTON(appState.sliderValuePercent)
+    return [parseFloat(stakedAmount).toFixed(2), (parseFloat(stakedAmount * dailyProfitPercent)).toFixed(2)];
+}
+
+async function stakeAndPoll(txBoc, payoutInterval, senderWallet, stakedAmount, returnedAmount) {
+    try {
+        const initData = window.appConfig.telegramWebApp.initData;
+        const stakeResult = await stake(initData, {
+            tx_boc: txBoc,
+            payout_interval: payoutInterval,
+            wallet: senderWallet,
+            staked_amount: stakedAmount,
+            returned_amount: returnedAmount
+        });
+
+        if (!stakeResult.isAvailable) {
+            const message = "Only one active stake is allowed";
+            const icon = "img/warning.svg";
+            const duration = 800;
+            playHapticWarning();
+            showSnackbar(message, icon, duration);
+        } else {
+            renderTransactions(stakeResult.transactions);
+            const ref = null;
+            const pollTransactions = async () => {
+                try {
+                    const result = await window.getUserSnapshot(initData, ref);
+                    console.log(`Pool transaction status. result=${JSON.stringify(result)}`)
+                    if (result && result.transactions) {
+                        renderTransactions(result.transactions);
+                    }
+                    const shouldPoll = result.transactions.some(tx => tx.status === TxStatus.pending);
+                    if (shouldPoll) {
+                        setTimeout(pollTransactions, 10_000);
+                    }
+                } catch (err) {
+                    console.error("Polling error:", err);
+                }
+            };
+            setTimeout(pollTransactions, 10_000);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function updateStakeButtonText(stakeButton, wallet) {
+    if (wallet) {
         stakeButton.textContent = "Stake"
     } else {
         stakeButton.textContent = "Connect wallet"
     }
+}
 
-    stakeButton.addEventListener("click", () => {
+async function isStakeAvailable() {
+    return await window.isStakeAvailable(window.telegramWebApp.initData)
+}
+
+function updateStakeButton(tonConnectUI, user) {
+    const stakeButton = document.getElementById("stakeBtn")
+    updateStakeButtonText(stakeButton, tonConnectUI.wallet)
+    function stakeButtonListener() {
+        console.log("stakeButtonListener click")
+        const isWalletConected = tonConnectUI.wallet
+        console.log('wallet ', isWalletConected, isWalletConected && isWalletConected.account.address)
         playHapticNavigation();
         if (isWalletConected) {
             (async () => {
                 try {
-                    const result = await sendTransaction(tonConnectUI, 0.01);
-                    if (result) {
-                        console.log("Tx result ", JSON.stringify(result))
+                    const isStkAvailable = await isStakeAvailable();
+                    console.log("isStakeAvailable=", isStkAvailable)
+                    if (!isStkAvailable) {
+                        const message = "Only one active stake is allowed";
+                        const icon = "img/warning.svg";
+                        const duration = 800;
+                        playHapticWarning();
+                        showSnackbar(message, icon, duration);
+                        return
+                    }
+                    const amount = 0.01;
+                    const payload = user.payload;
+                    const txBoc = await sendTransaction(tonConnectUI, amount, payload);
+                    // const txBoc = "te6cckEBBAEAtgAB5YgBuvb4cMDX4ZFuRakLWhbBHYCx9buzkfN9oNn0SjB9/0ADm0s7c///+ItFZA5IAAAAfFouCHmtjC+ql+83LVIKd+yMpFw0e3oGK36CRM/8+Xd7FwbIM6qQI3gPsaAJSc9NJGo2EO2f3JsRmGfpMW7GUA0BAgoOw8htAwIDAAAAZkIAbWCkBbDahLD3RuEhuE4EzaqpIQFGjt3G6Idph04YoROcxLQAAAAAAAAAAAAAAAAAAIOv1IY=";
+                    console.log("Tx boc ", txBoc)
+                    const payoutInterval = appState.profitInterval;
+                    const senderWalet = tonConnectUI.wallet.account.address;
+                    const localStake = calculateStakedReturnedAmount(user.dailyProfitPercent);
+                    const stakedAmount = localStake[0];
+                    const returnedAmount = localStake[1];
+                    if (txBoc) {
+                        console.log("Tx result ", JSON.stringify(txBoc))
                         const message = "Success";
                         const icon = "img/completed.svg"
                         playHapticSuccess();
                         showSnackbar(message, icon);
-                        return result;
+                        await stakeAndPoll(txBoc.boc, payoutInterval, senderWalet, stakedAmount, returnedAmount)
                     } else {
-                        const message = "Error. Try again";
+                        const message = "Transaction cancelled. Try again";
                         const icon = "img/error.svg"
                         playHapticError();
                         showSnackbar(message, icon);
                     }
                 } catch (e) {
+                    const message = "Error. Try again";
+                    const icon = "img/error.svg"
+                    playHapticError();
+                    showSnackbar(message, icon);
                     console.error(e);
                 }
             })();
         } else {
             tonConnectUI.openModal();
         }
-    })
+    }
+
+    stakeButton.addEventListener("click", stakeButtonListener)
     stakeButton.style.display = 'block'
 }
 
-function initUi() {
-    initSlider();
+function initUi(user) {
+    initSlider(user.dailyProfitPercent);
+    renderTransactions(user.transactions)
 
     const tabs = document.querySelectorAll(".tab-item");
     const payoutProfitButtons = document.querySelectorAll(".interval-btn");
-    
+
     function updateTabsSelection() {
         tabs.forEach((tab, index) => {
             if (index === appState.selectedTab) {
-              tab.classList.add("active");
+                tab.classList.add("active");
             } else {
-              tab.classList.remove("active");
+                tab.classList.remove("active");
             }
-          });
+        });
     }
 
     function updatePayoutProfitButtonsSelection() {
@@ -179,7 +366,7 @@ function initUi() {
             }
         });
     }
-    
+
     tabs.forEach((tab, index) => {
         tab.addEventListener("click", () => {
             playHapticNavigation();
@@ -187,11 +374,17 @@ function initUi() {
                 appState.selectedTab = index;
                 updateTabsSelection();
             } else if (index === tabItems.get("community")) {
-                // Community
-            } else {
+                // todo open community
+            } else if (index === tabItems.get("pool")) {
+                const message = "Available soon";
+                const icon = null;
+                const duration = 800;
+                playHapticWarning();
+                showSnackbar(message, icon, duration);
+            }
+            else {
                 console.log(`Uknown tab with index=${index} clicked`)
             }
-            
         });
     });
 
@@ -202,34 +395,58 @@ function initUi() {
                 appState.profitInterval = index;
                 updatePayoutProfitButtonsSelection()
             } else if (index === profitIntervals.get("week")) {
-                appState.profitInterval = index;
-                updatePayoutProfitButtonsSelection()
+                const message = "Available soon";
+                const icon = null;
+                playHapticWarning();
+                showSnackbar(message, icon);
             } else if (index === profitIntervals.get("month")) {
-                const message = "Not available";
-                const icon = "img/warning.svg"
+                const message = "Available soon";
+                const icon = null;
                 playHapticWarning();
                 showSnackbar(message, icon);
             } else if (index === profitIntervals.get("year")) {
-                const message = "Not available";
-                const icon = "img/warning.svg"
+                const message = "Available soon";
+                const icon = null;
                 playHapticWarning();
                 showSnackbar(message, icon);
             }
         })
     });
 
-    updateTabsSelection()  
-    updatePayoutProfitButtonsSelection();  
+    updateTabsSelection()
+    updatePayoutProfitButtonsSelection();
 }
 
+
+
 window.onload = function () {
-    initUi();
-    const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-        manifestUrl: 'https://sklych.github.io/door/tonconnect-manifest.json', // todo replace
-        language: 'en',
-    });
-    tonConnectUI.onStatusChange(wallet => {
-        updateStakeButton(tonConnectUI)
-    })
+    (async () => {
+        try {
+            const stakeButton = document.getElementById("stakeBtn")
+            const initData = window.appConfig.telegramWebApp.initData;
+            const ref = window.appConfig.telegramWebApp.initDataUnsafe.start_param;
+            const result = await window.getUserSnapshot(initData, ref);
+            if (result) {
+                console.log(`Load user=${JSON.stringify(result)}`)
+                initUi(result);
+                const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+                    manifestUrl: 'https://sklych.github.io/door/tonconnect-manifest.json', // todo replace
+                    language: 'en',
+                });
+                updateStakeButton(tonConnectUI, result)
+                tonConnectUI.onStatusChange(wallet => {
+                    updateStakeButtonText(stakeButton, wallet)
+                })
+            } else {
+                throw Error("No data loaded")
+            }
+        } catch (e) {
+            const message = "Could not load data. Reload MiniApp";
+            const icon = "img/error.svg"
+            playHapticError();
+            showSnackbar(message, icon);
+            console.error(e);
+        }
+    })();
 };
 
